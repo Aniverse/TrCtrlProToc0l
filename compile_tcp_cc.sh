f8bad8184072e19dc2cf407a0d2b661b86cf44ec
Aniverse <369842286@qq.com>
2019-04-25 15:33:14 +0800

    Enable tsunami for kernel 4.9

--- compile_tcp_cc.sh
+++ compile_tcp_cc.sh
@@ -5,7 +5,7 @@
 #
 # bash <(curl -s https://raw.githubusercontent.com/Aniverse/TrCtrlProToc0l/master/compile_tcp_cc.sh) tsunami nanqinlang bbrplus
 script_update=2019.04.25
-script_version=1.0.2
+script_version=1.0.3
 
 tcp_cc=$1
 #[[ -n $Outputs ]] && OutputLOG=">> $OutputLOG 2>&1"
@@ -16,7 +16,7 @@ kernel_v=$(uname -r | cut -d- -f1)
 kernel_v2=$(uname -r | cut -d- -f1 | cut -d. -f1-2)
 
 version_ge $kernel_v 4.9.3 && [[ $tcp_cc == nanqinlang ]] && supported_list="4.9 4.10 4.11 4.12 4.13 4.14 4.15 4.16 4.17 4.18 4.19 4.20 5.0"
-version_ge $kernel_v 4.10  && [[ $tcp_cc == tsunami ]]    && supported_list="4.10 4.11 4.12 4.13 4.14 4.15 4.16 4.17 4.18 4.19 4.20 5.0"
+version_ge $kernel_v 4.9.3  && [[ $tcp_cc == tsunami ]]    && supported_list="4.9 4.10 4.11 4.12 4.13 4.14 4.15 4.16 4.17 4.18 4.19 4.20 5.0"
 version_ge $kernel_v 4.14  && [[ $tcp_cc == bbrplus ]]    && supported_list="4.14 4.15 4.16 4.17 4.18 4.19 4.20 5.0"
 
 mkdir compile_tcp_cc
